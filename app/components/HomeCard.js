8ad64a7aca85b6596aa117ed523e5a1f26c07bdd
zahidalidev <m.zahidalidev@gmail.com>
2021-07-01 06:17:39 +0500

    home screen complete

---
+++ app/components/HomeCard.js
@@ -0,0 +1,162 @@
+import React from 'react';
+import { StyleSheet, View, Text, TouchableOpacity } from 'react-native';
+import { RFPercentage } from 'react-native-responsive-fontsize';
+import { MaterialCommunityIcons, Ionicons, Entypo, AntDesign } from "@expo/vector-icons"
+
+import colors from '../config/colors';
+
+function HomeCard({ data, index, showDetails }) {
+    return (
+        <View style={{ margin: RFPercentage(1), justifyContent: "center", alignItems: "center" }} >
+            {/* card header */}
+            <View style={{
+                shadowColor: "#000",
+                shadowOffset: {
+                    width: 0,
+                    height: 2,
+                },
+                shadowOpacity: 0.23,
+                shadowRadius: 2.62,
+                elevation: 4,
+                flexDirection: "row",
+                width: "90%",
+                backgroundColor: colors.white,
+                padding: RFPercentage(2),
+                justifyContent: "space-between",
+                alignItems: "center",
+                borderRadius: 5,
+                marginTop: index == 0 ? RFPercentage(3.4) : RFPercentage(1),
+            }} >
+                <View style={{ width: "85%", flexDirection: "column" }} >
+                    <View>
+                        <Text style={{ fontSize: RFPercentage(3.3) }} >{data.title}</Text>
+                    </View>
+                    <View style={{ flexDirection: "row", marginTop: RFPercentage(0.6) }} >
+                        <Ionicons name="md-people" size={RFPercentage(2.6)} color={colors.black} />
+                        <Text style={{ marginLeft: RFPercentage(1), fontSize: RFPercentage(2.2), color: colors.grey }} >{data.places}</Text>
+                    </View>
+                </View>
+                <TouchableOpacity onPress={() => showDetails()} activeOpacity={0.5} >
+                    <MaterialCommunityIcons name="chevron-right" size={RFPercentage(4)} color={colors.black} />
+                </TouchableOpacity>
+            </View>
+            {/* card details */}
+
+            {
+                data.showDetails ?
+
+                    <View style={{
+                        shadowColor: "#000",
+                        shadowOffset: {
+                            width: 0,
+                            height: 2,
+                        },
+                        shadowOpacity: 0.23,
+                        shadowRadius: 2.62,
+                        elevation: 4,
+                        flexDirection: "column",
+                        width: "90%",
+                        backgroundColor: colors.white,
+                        padding: RFPercentage(2),
+                        justifyContent: "center",
+                        alignItems: "center",
+                        borderRadius: 5,
+                        marginTop: RFPercentage(1),
+                    }} >
+                        {
+                            data.details.map((item, index) =>
+                                <View key={index} style={{
+                                    flexDirection: "row",
+                                    width: "90%",
+                                    backgroundColor: colors.white,
+                                    justifyContent: "center",
+                                    alignItems: "center",
+                                    marginTop: RFPercentage(2)
+                                }} >
+                                    <View style={{
+                                        shadowColor: "#000",
+                                        shadowOffset: {
+                                            width: 0,
+                                            height: 2,
+                                        },
+                                        shadowOpacity: 0.23,
+                                        shadowRadius: 2.62,
+                                        elevation: 4,
+                                        backgroundColor: colors.white,
+                                        padding: 10,
+                                        marginRight: RFPercentage(2)
+                                    }} >
+                                        {
+                                            item.name1 === "cancel" ?
+                                                <MaterialCommunityIcons name="cancel" color={colors.danger} size={RFPercentage(3.7)} />
+                                                :
+                                                (
+                                                    item.name1 === "check" ?
+                                                        <AntDesign name="check" color="green" size={RFPercentage(3.7)} />
+                                                        :
+                                                        <Entypo name="cross" color={colors.black} size={RFPercentage(3.7)} />
+                                                )
+                                        }
+                                    </View>
+                                    <View style={{
+                                        shadowColor: "#000",
+                                        shadowOffset: {
+                                            width: 0,
+                                            height: 2,
+                                        },
+                                        shadowOpacity: 0.23,
+                                        shadowRadius: 2.62,
+                                        elevation: 4,
+                                        backgroundColor: colors.white,
+                                        padding: 10,
+                                        marginRight: RFPercentage(2)
+                                    }} >
+                                        {
+                                            item.name2 === "cancel" ?
+                                                <MaterialCommunityIcons name="cancel" color={colors.danger} size={RFPercentage(3.7)} />
+                                                :
+                                                (
+                                                    item.name2 === "check" ?
+                                                        <AntDesign name="check" color="green" size={RFPercentage(3.7)} />
+                                                        :
+                                                        <Entypo name="cross" color={colors.black} size={RFPercentage(3.7)} />
+                                                )
+                                        }
+                                    </View>
+                                    <View style={{
+                                        shadowColor: "#000",
+                                        shadowOffset: {
+                                            width: 0,
+                                            height: 2,
+                                        },
+                                        shadowOpacity: 0.23,
+                                        shadowRadius: 2.62,
+                                        elevation: 4,
+                                        backgroundColor: colors.white,
+                                        padding: 10
+                                    }} >
+                                        {
+                                            item.name3 === "cancel" ?
+                                                <MaterialCommunityIcons name="cancel" color={colors.danger} size={RFPercentage(3.7)} />
+                                                :
+                                                (
+                                                    item.name3 === "check" ?
+                                                        <AntDesign name="check" color="green" size={RFPercentage(3.7)} />
+                                                        :
+                                                        <Entypo name="cross" color={colors.black} size={RFPercentage(3.7)} />
+                                                )
+                                        }
+                                    </View>
+                                </View>
+
+                            )
+                        }
+
+                    </View> : null}
+
+        </View>
+
+    );
+}
+
+export default HomeCard;
--- app/screens/Deconnexion.js
+++ app/screens/Deconnexion.js
@@ -1,14 +1,96 @@
 import React from 'react';
-import { StyleSheet, View, StatusBar } from 'react-native';
+import { StyleSheet, View, StatusBar, Text, ScrollView } from 'react-native';
 import { Appbar } from 'react-native-paper';
 import { RFPercentage } from 'react-native-responsive-fontsize';
 import CalendarStrip from 'react-native-calendar-strip';
+import { MaterialCommunityIcons, Ionicons } from "@expo/vector-icons"
 
 import colors from '../config/colors';
 import moment from 'moment';
+import HomeCard from '../components/HomeCard';
+import { useState } from 'react';
 
 function Deconnexion(props) {
 
+    const [cardData, setCardData] = useState([
+        {
+            id: 0,
+            title: "Salle 1",
+            places: "4 places dispoibles",
+            showDetails: false,
+            details: [
+                {
+                    name1: "cancel",
+                    name2: "check",
+                    name3: "cross",
+                },
+                {
+                    name1: "check",
+                    name2: "cross",
+                    name3: "cross",
+                },
+                {
+                    name1: "check",
+                    name2: "cancel",
+                    name3: "check",
+                },
+                {
+                    name1: "cancel",
+                    name2: "check",
+                    name3: "cross",
+                },
+            ]
+        },
+        {
+            id: 0,
+            title: "Salle 2",
+            places: "7 places dispoibles",
+            showDetails: false,
+            details: [
+                {
+                    name1: "cancel",
+                    name2: "cross",
+                    name3: "cancel",
+                },
+                {
+                    name1: "cancel",
+                    name2: "check",
+                    name3: "cross",
+                },
+                {
+                    name1: "check",
+                    name2: "cross",
+                    name3: "cross",
+                },
+                {
+                    name1: "check",
+                    name2: "cancel",
+                    name3: "check",
+                },
+                {
+                    name1: "cancel",
+                    name2: "check",
+                    name3: "cross",
+                },
+                {
+                    name1: "check",
+                    name2: "cancel",
+                    name3: "check",
+                },
+                {
+                    name1: "cancel",
+                    name2: "check",
+                    name3: "cross",
+                },
+            ]
+        },
+    ])
+
+    const handleShowDetails = index => {
+        let tempDetails = [...cardData];
+        tempDetails[index].showDetails = !tempDetails[index].showDetails;
+        setCardData(tempDetails)
+    }
 
     return (
         <View style={styles.container}>
@@ -17,46 +99,62 @@ function Deconnexion(props) {
                 <Appbar.Action onPress={() => props.navigation.openDrawer()} icon="menu" />
             </Appbar>
 
-            <View style={{ flex: 1 }}>
-                <CalendarStrip
-                    locale={
-                        {
-                            name: "fr",
-                            config: {
-                                months: 'Janvier_Février_Mars_Avril_Mai_Jui_Juillet_Août_Septembre_Octobre_Novembre_Décembre'.split(
-                                    '_'
-                                ),
-                                monthsShort: 'Janv_Févr_Mars_Avr_Mai_Juin_Juil_Août_Sept_Oct_Nov_Déc'.split(
-                                    '_'
-                                ),
-                                weekdays: 'Dimanche_Lundi_Mardi_Mercredi_Jeudi_Vendredi_Samedi'.split('_'),
-                                weekdaysShort: 'D_L_M_M_J_V_S'.split('_'),
-                                weekdaysMin: 'DW_LE_Me_Mr_J_tV_yS'.split('_'),
-                            }
+            <CalendarStrip
+                locale={
+                    {
+                        name: "fr",
+                        config: {
+                            months: 'Janvier_Février_Mars_Avril_Mai_Jui_Juillet_Août_Septembre_Octobre_Novembre_Décembre'.split(
+                                '_'
+                            ),
+                            monthsShort: 'Janv_Févr_Mars_Avr_Mai_Juin_Juil_Août_Sept_Oct_Nov_Déc'.split(
+                                '_'
+                            ),
+                            weekdays: 'Dimanche_Lundi_Mardi_Mercredi_Jeudi_Vendredi_Samedi'.split('_'),
+                            weekdaysShort: 'D_L_M_M_J_V_S'.split('_'),
+                            weekdaysMin: 'DW_LE_Me_Mr_J_tV_yS'.split('_'),
                         }
                     }
-                    scrollable
-                    style={{ height: 120, }}
-                    calendarHeaderFormat={"MMM"}
-                    showMonth={true}
-                    // dayComponentHeight={50}
-                    calendarColor={colors.primary}
-                    calendarHeaderStyle={{ color: 'white', fontSize: 20 }}
-                    dateNameStyle={{ fontSize: 20, marginTop: 10, color: colors.white }}
-                    dateNumberStyle={{ flex: 1, color: 'white', fontSize: 18, marginBottom: 10, paddingLeft: 10, padding: 5, paddingRight: 10 }}
-                    selectedDate={moment(new Date())}
-                    highlightDateNameStyle={{ fontSize: 18, marginTop: 10, color: colors.white }}
-                    highlightDateNumberStyle={{ marginBottom: 15, backgroundColor: colors.white, fontSize: 18, paddingLeft: 10, padding: 5, paddingRight: 10, borderRadius: 100, color: colors.black }}
-                    iconContainer={{ flex: 0.1 }}
-                    iconStyle={{ tintColor: colors.white }}
-                />
+                }
+                scrollable
+                style={{ height: 100, marginTop: -20 }}
+                calendarHeaderFormat={"MMM"}
+                showMonth={true}
+                // dayComponentHeight={50}
+                calendarColor={colors.primary}
+                calendarHeaderStyle={{ color: 'white', fontSize: 20 }}
+                dateNameStyle={{ fontSize: 20, marginTop: 10, color: colors.white }}
+                dateNumberStyle={{ flex: 1, color: 'white', fontSize: 18, marginBottom: 10, paddingLeft: 10, padding: 5, paddingRight: 10 }}
+                selectedDate={moment(new Date())}
+                highlightDateNameStyle={{ fontSize: 18, marginTop: 10, color: colors.white }}
+                highlightDateNumberStyle={{ marginBottom: 15, backgroundColor: colors.white, fontSize: 18, paddingLeft: 10, padding: 5, paddingRight: 10, borderRadius: 100, color: colors.black }}
+                iconContainer={{ flex: 0.1 }}
+                iconStyle={{ tintColor: colors.white }}
+            />
+
+            <View style={{ marginTop: RFPercentage(2), flexDirection: "row", alignItems: "center", justifyContent: "space-evenly" }} >
+                <Text style={{ fontSize: RFPercentage(2.2), color: colors.black }} >1er etage</Text>
+                <Text style={{ fontSize: RFPercentage(2.2), color: colors.grey }} >2eme etage</Text>
+                <Text style={{ fontSize: RFPercentage(2.2), color: colors.grey }} >4eme etage</Text>
             </View>
+
+
+            {/* Bottom cards */}
+            <ScrollView>
+                {
+                    cardData.map((item, index) =>
+                        <HomeCard key={index} index={index} showDetails={() => handleShowDetails(index)} data={item} />
+                    )
+                }
+            </ScrollView>
         </View>
     );
 }
 
 const styles = StyleSheet.create({
     container: {
+        flex: 1,
+        backgroundColor: colors.white
         // marginTop: Constants.statusBarHeight
     }
 })
