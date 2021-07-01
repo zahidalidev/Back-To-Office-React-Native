4f1d6a47bdd842c0d9687a68720582ee29ab3176
zahidalidev <m.zahidalidev@gmail.com>
2021-07-01 05:14:34 +0500

    white arrow

--- app/screens/Deconnexion.js
+++ app/screens/Deconnexion.js
@@ -5,8 +5,11 @@ import { RFPercentage } from 'react-native-responsive-fontsize';
 import CalendarStrip from 'react-native-calendar-strip';
 
 import colors from '../config/colors';
+import moment from 'moment';
 
 function Deconnexion(props) {
+
+
     return (
         <View style={styles.container}>
             <StatusBar backgroundColor={colors.primary} />
@@ -33,15 +36,36 @@ function Deconnexion(props) {
                         }
                     }
                     scrollable
-                    style={{ height: 120 }}
+                    style={{ height: 120, }}
                     calendarHeaderFormat={"MMM"}
                     showMonth={true}
                     // dayComponentHeight={50}
                     calendarColor={colors.primary}
-                    calendarHeaderStyle={{ color: 'white' }}
-                    dateNameStyle={{ color: 'white', fontSize: 20, marginTop: 10 }}
-                    dateNumberStyle={{ flex: 1, color: 'white', fontSize: 20, marginTop: 10 }}
+                    calendarHeaderStyle={{ color: 'white', fontSize: 20 }}
+                    dateNameStyle={{ fontSize: 20, marginTop: 10, color: colors.white }}
+                    dateNumberStyle={{ flex: 1, color: 'white', fontSize: 18, marginBottom: 10, paddingLeft: 10, padding: 5, paddingRight: 10 }}
+                    selectedDate={moment(new Date())}
+
+                    // markedDates={
+                    //     [
+                    //         {
+                    //             date: "2021-07-02T00:06:08.323Z",
+                    //             dots: [
+                    //                 {
+                    //                     color: 'red', fontSize: 20, marginTop: 10,
+                    //                     selectedColor: "red",
+                    //                 },
+                    //             ],
+                    //         },
+
+                    //     ]
+                    // }
+                    // highlightDateContainerStyle={{ backgroundColor: `#${(`#00000${(Math.random() * (1 << 24) | 0).toString(16)}`).slice(-6)}` }}
+                    highlightDateNameStyle={{ fontSize: 18, marginTop: 10, color: colors.white }}
+                    highlightDateNumberStyle={{ marginBottom: 15, backgroundColor: colors.white, fontSize: 18, paddingLeft: 10, padding: 5, paddingRight: 10, borderRadius: 100, color: colors.black }}
                     iconContainer={{ flex: 0.1 }}
+                    iconStyle={{ tintColor: colors.white }}
+                // iconStyle
                 />
             </View>
         </View>
